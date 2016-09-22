6e7936f5e1ed3ff65971c6a383ded2f4086f9e34
gjowpractice <gjow@harris.com>
2016-09-22 09:04:12 -0400

    read function

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -97,3 +97,22 @@ func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte
     return nil, nil
 }
 
+// adding a read function
+func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
+
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
+}
+
